60ac0e64c58f3adc370e5688015cbe6b57e12602
doowrag22 <100243428+doowrag22@users.noreply.github.com>
2022-02-23 13:53:31 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+var myAge = 40;
+//creating variable myAge and marking as my actual age 40
+
+var earlyYears = 2;
+//creating earlyYears variable
+
+earlyYears *= 10.5;
+//multiplying earlyYears by 10.5 and reassigning that value to earlyYears
+
+laterYears = myAge - 2;
+//taking earlyYears out of my age
+
+laterYears *= 4;
+
+myAgeInDogYears = earlyYears + laterYears;
+//calculating my age in dogyears
+
+myName = 'Bryan'.toLowerCase();
+//creating myName variable and moving to all lowercase
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
